c190490076d42bd8c451ae0c5baa46d296d5ba85
Aditya062005 <145774732+Aditya062005@users.noreply.github.com>
2023-10-11 18:18:29 +0530

    Create aditya.cpp

---
+++ aditya.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int row =0;row<3;row++){
+        for(int col=0;col<5;col++){
+            if(row==0|| row==2||col==0||col==4){
+                cout<<"*";  
+
+            }
+            else{
+                cout<< " ";
+
+            }
+            cout<<endl;
+        }
+    }
+}
+
+
+
