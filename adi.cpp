7defc51b45ab8e9527513ddf1c347793c32c83d7
Aditya062005 <145774732+Aditya062005@users.noreply.github.com>
2023-10-11 18:20:53 +0530

    Create adi.cpp

---
+++ adi.cpp
@@ -0,0 +1,30 @@
+include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    if (isPrime(num)) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
