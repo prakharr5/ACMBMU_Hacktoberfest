5a3ddf2f44a3629ee8911f943431783c9a52b0ee
utkarshtonk <141713381+utkarshtonk@users.noreply.github.com>
2023-10-12 10:41:02 +0530

    Create bt.java

---
+++ bt.java
@@ -0,0 +1,18 @@
+class bt
+{
+    public static void main(String []args)
+    {
+        int rows =5;
+        char ch = 65;
+        for(int i=1;i<=rows;i++)
+        {
+            
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
