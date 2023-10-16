65e066ff90231d5439eab06f42f51f03bc75fa97
GauravG2207 <145753089+GauravG2207@users.noreply.github.com>
2023-10-16 22:29:54 +0530

    Create jstest.js

---
+++ jstest.js
@@ -0,0 +1,38 @@
+/*Data types:
+undefined, null, boolean, string, symbol,number,and object*/
+var myName = "Gaurav"
+
+myName = 8
+
+let ourName = "Ties"
+const pi = 3.14
+
+
+var a ; //declaring 
+var b = 2;//assignment
+console.log(b);
+a = 7;
+b = a;
+
+
+console.log(b);
+// console.log(a)
+
+var a = 9;
+console.log(a)
+
+/*operations*/
+var num1 = 5;
+var num2 = 10;
+var string = "Hello World";
+
+num1 = num1 +1;
+num2 = num2 +5;
+string = string + "String!";
+console.log(num1,num2,string)
+/* Java Script is case sensitive*/
+console.log(num1/num2) 
+num1++;num1-- //increament and decrement works the same as cpp
+
+var len_string = string.length;//method to find out the lenght 
+console.log(len_string)
